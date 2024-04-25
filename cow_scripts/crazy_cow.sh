0033f68b6d85920e6a1580fdd04e741371e05130
bengaudry <bengaudry@outlook.fr>
2024-04-25 18:51:03 +0200

    qdjk

--- cow_scripts/crazy_cow.sh
+++ cow_scripts/crazy_cow.sh
@@ -3,27 +3,24 @@
 # Ce script attend trois arguments a, b, c représentant une équation de la forme "ax^2+bx+c=0"
 
 # On quitte avec une erreur si le nombre d'arguments est différent de 3
-if [ $# -ne 3 ]
-then
-  echo "Trois argument requis (a,b,c -> ax^2+bx+c=0)"
+if [ "$#" -ne 3 ]; then
+  echo "Trois arguments requis (a, b, c -> ax^2+bx+c=0)"
   exit 1
 fi
 
-a=$1
-b=$2
-c=$3
+a="$1"
+b="$2"
+c="$3"
 
-delta=$(($b*$b-4*$a*$c))
-if [ $delta -gt 0 ]
-then
+delta=$((b*b-4*a*c))
+if [ "$delta" -gt 0 ]; then
   rac=$(bc -l <<<"sqrt($delta)")
-  x1=$((-$b-$rac/(2*$a)))
-  x2=$((-$b+$rac/(2*$a)))
-  cowsay "$1 et $2"
-elif [ $delta -eq 0 ]
-then
-  sol=$((-$b/2*$a))
-  cowsay $sol
+  x1=$(bc -l <<<"(-$b-$rac)/(2*$a)")
+  x2=$(bc -l <<<"(-$b+$rac)/(2*$a)")
+  echo "Les solutions sont x1 = $x1 et x2 = $x2"
+elif [ "$delta" -eq 0 ]; then
+  sol=$(bc -l <<<"-$b/(2*$a)")
+  echo "La solution double est x = $sol"
 else
-  cowsay "Pas de solution réelle"
+  echo "Pas de solution réelle"
 fi
